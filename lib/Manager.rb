4c1c8492444fd5a89914a3bf9a2fee04db5fa39e
AlveeM <alvee.akand@outlook.com>
2020-06-26 19:39:23 -0400

    Done.

--- lib/Manager.rb
+++ lib/Manager.rb
@@ -1,3 +1,35 @@
 class Manager
+  attr_reader :name
+  attr_accessor :department, :age 
 
+  @@all = []
+
+  def initialize(name, department, age)
+    @name = name
+    @department = department
+    @age = age
+    @@all << self
+  end
+
+  def employees
+    Employee.all.select { |employee| employee.manager == self}
+  end
+
+  def hire_employee(name, salary)
+    Employee.new(name, salary, self)
+  end
+
+  def self.all_departments
+    self.all.map { |manager| manager.department }.uniq
+  end
+
+  def self.average_age
+    age_total = self.all.reduce(0) {|total, manager| total += manager.age }
+    manager_total = self.all.length
+    age_total.fdiv(manager_total)
+  end
+
+  def self.all 
+    @@all
+  end
 end
