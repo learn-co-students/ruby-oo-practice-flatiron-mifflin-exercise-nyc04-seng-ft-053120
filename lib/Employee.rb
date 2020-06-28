3fa4ce067f3036beb3c982c613f2f847c6890f62
sarahmr63089 <sarahrose63089@gmail.com>
2020-06-28 18:02:19 -0400

    Done.

--- lib/Employee.rb
+++ lib/Employee.rb
@@ -1,3 +1,30 @@
 class Employee
+    attr_reader :name, :salary, :manager_name
+    @@all = []
 
+    def initialize(name, salary, manager_name)
+        @name = name
+        @salary = salary
+        @manager_name = manager_name.name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.paid_over(amount)
+        all.select { |employee| employee.salary > amount}
+    end
+
+    def self.find_by_department(department)
+        # departments are in the manager class, get manager for department
+        manager = Manager.all.find { |manager| manager.department == department }
+        # get first employee for manager
+        all.find { |employee| employee.manager_name == manager.name }
+    end
+
+    def tax_bracket
+        Employee.all.select { |employee| employee.salary.between?(self.salary - 1000, self.salary + 1000) }
+    end
 end
--- lib/Manager.rb
+++ lib/Manager.rb
@@ -1,3 +1,33 @@
 class Manager
 
+    attr_accessor :name, :department, :age
+    @@all = []
+
+    def initialize(name, department, age)
+        @name = name
+        @department = department
+        @age = age
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.all_departments
+        all.map { |manager| manager.department }
+    end
+
+    def self.average_age
+        sum_of_ages = all.sum { |manager| manager.age }
+        sum_of_ages / all.count.to_f
+    end
+
+    def employees
+        Employee.all.select { |employee| employee.manager_name == self.name }
+    end
+
+    def hire_employee(name, salary)
+        Employee.new(name, salary, self)
+    end
 end
